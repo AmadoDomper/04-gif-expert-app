d8bf29c4cd2b000a7cffc8005659a374b688d0ca
Amado Domper <amado.domper@gmail.com>
2022-04-05 19:18:19 -0500

    End AddCategory.js unit testing

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,7 +4,12 @@ import '@testing-library/jest-dom';
 
 describe("Pruebas en <AddCategory />", () => {
   const setCategories = jest.fn();
-  const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+  beforeEach( () => {
+    jest.clearAllMocks();
+    wrapper = shallow(<AddCategory setCategories={setCategories} />);
+  })
 
   test("should show it correctly", () => {
     expect(wrapper).toMatchSnapshot();
@@ -24,7 +29,23 @@ describe("Pruebas en <AddCategory />", () => {
   test('should not post the information with submit.', () => {
       wrapper.find('form').simulate('submit', { preventDefault(){} });
 
-    //   expect( setCategories ).not.toHaveBeenCalled();
+      expect( setCategories ).not.toHaveBeenCalled();
   })
+
+  test('should call setCategories and clean box text', () => {
+    const value = "Goku";
+
+    wrapper.find("input").simulate("change", {target: { value }});
+
+    wrapper.find('form').simulate('submit', { preventDefault(){} });
+
+    expect( setCategories ).toHaveBeenCalled();
+    expect( setCategories ).toHaveBeenCalledTimes(1);
+    expect( setCategories ).toHaveBeenCalledWith(expect.any(Function));
+
+    expect(wrapper.find('input').prop('value')).toBe('');
+
+  })
+  
   
 });
